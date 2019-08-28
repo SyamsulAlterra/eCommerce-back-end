ac576d32f12abadabf0ed80b104946f48b7db84d
Syamsul <syamsul@alterra.id>
2019-08-28 19:26:58 +0700

    fresh key

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ sudo docker stop syamsuldocker/backend_automate
 sudo docker rm syamsuldocker/backend_automate
 sudo docker rmi syamsuldocker/backend_automate
 sudo docker pull syamsuldocker/backend_automate
-sudo docker stop 9e57ccefccf3
-sudo docker run -d -p 5001:5001 syamsuldocker/backend_automate:latest
+# sudo docker stop 9e57ccefccf3
+# sudo docker run -d -p 5001:5001 syamsuldocker/backend_automate:latest
