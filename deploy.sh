5bcbc1a6040750101503d28d67601a668ad3c5ba
Syamsul <syamsul@alterra.id>
2019-08-28 14:48:50 +0700

    change port

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ sudo docker stop syamsuldocker/image_test
 sudo docker rm syamsuldocker/image_test
 sudo docker rmi syamsuldocker/image_test
 sudo docker pull syamsuldocker/image_test
-sudo docker run -d -p 5001:5001 syamsuldocker/image_test:latest
+sudo docker run -d -p 8000:8000 syamsuldocker/image_test:latest
