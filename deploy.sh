ec8c156454f0205ca586bb070c5b522045e393d3
Syamsul <syamsul@alterra.id>
2019-08-28 12:49:04 +0700

    change travis

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop helloworld
 docker rm helloworld
-docker rmi aprynur/helloworld
-docker run -d --name helloworld -p 5000:5000 aprynur/helloworld:latest
+docker rmi syamsuldocker/helloworld
+docker run -d --name helloworld -p 5000:5000 syamsuldocker/helloworld:latest
